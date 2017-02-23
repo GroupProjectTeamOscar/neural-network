d5f8f42127010a46db2feee2445dff7fdefeaa19
Elizabeth Barnes <beth.m.barnes@gmail.com>
2017-02-23 11:41:24 +0000

    use mumsnet data

--- contrib_learn_tutorial.py
+++ contrib_learn_tutorial.py
@@ -8,22 +8,29 @@ import pandas as pd
 import tensorflow as tf
 
 tf.logging.set_verbosity(tf.logging.INFO)
-COLUMNS = ["crim", "zn", "indus", "nox", "rm", "age","dis", "tax", "ptratio", "medv"]
-FEATURES = ["crim", "zn", "indus", "nox", "rm",
-            "age", "dis", "tax", "ptratio"]
-LABEL = "medv"
 
-training_set = pd.read_csv("boston_train.csv", skipinitialspace=True,
-                           skiprows=1, names=COLUMNS)
-test_set = pd.read_csv("boston_test.csv", skipinitialspace=True,
-                       skiprows=1, names=COLUMNS)
-prediction_set = pd.read_csv("boston_predict.csv", skipinitialspace=True,
-                             skiprows=1, names=COLUMNS)
+data = pd.read_csv("SubstanceAbuseOutput.csv")
+df=data.loc[1:, :]
+df['label'] = np.where(((df['hour']>21)|(df['hour']<6)),1,0)
+df=df.drop(['hour'], axis=1)
+
+COLUMNS = ['%s_'%k for k in range(0,10004)]
+COLUMNS.append('label')
+df.columns = COLUMNS
+training_set = df.loc[:2000]
+test_set = df.loc[2000:]
+
+FEATURES = train_set.columns.drop(['0_','label'],axis=1)
+LABEL = "label"
+
+
 feature_cols = [tf.contrib.layers.real_valued_column(k)
                   for k in FEATURES]
-regressor = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
+
+classifier = tf.contrib.learn.DNNRegressor(feature_columns=feature_cols,
                                           hidden_units=[10, 10],
-                                          model_dir="/tmp/boston_model")
+                                          n_classes=2,
+                                          model_dir="/tmp/substance_abuse_predict_night")
 
 def input_fn(data_set):
   feature_cols = {k: tf.constant(data_set[k].values, shape=[data_set[k].size, 1])
@@ -31,10 +38,9 @@ def input_fn(data_set):
   labels = tf.constant(data_set[LABEL].values, shape=[data_set[LABELatom].size, 1])
   return feature_cols, labels
 
-training_set[LABEL].values.shape
 
-regressor.fit(input_fn=lambda: input_fn(training_set), steps=5000)
+classifier.fit(input_fn=lambda: input_fn(training_set), steps=5000)
 
-ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
+ev = classifier.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
 loss_score = ev["loss"]
 print("Loss: {0:f}".format(loss_score))
